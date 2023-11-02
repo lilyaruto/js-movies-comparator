51e35b778e31c7fb7d1878fe051b76685f109f85
Yaruto <neo960211@gmail.com>
2023-11-02 17:14:30 +0100

    Fetching data test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async() => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "80f0e4df",
+            s: "avengers"
+        }
+    });
+
+    console.log(response.data)
+}
+
+fetchData();
