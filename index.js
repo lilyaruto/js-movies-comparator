6e940965487a18a75dfdd7d717d666d170365406
Yaruto <neo960211@gmail.com>
2023-11-02 17:36:09 +0100

    Added delaying of search input

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const fetchData = async() => {
+const fetchData = async(searchInput) => {
     const response = await axios.get("http://www.omdbapi.com/", {
         params: {
             apikey: "80f0e4df",
-            s: "avengers"
+            s: searchInput
         }
     });
-
     console.log(response.data)
 }
 
-fetchData();
+let timeoutId;
+const onInput = (event) => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value)
+    }, 1000)
+}
+
+const input = document.getElementById("search-bar");
+input.addEventListener("input", onInput);
