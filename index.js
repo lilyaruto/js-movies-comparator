79fa7405d305f8e178608978b3c558408b76f151
Yaruto <neo960211@gmail.com>
2023-12-19 01:44:07 +0100

    Rendering movie card

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ root.innerHTML = `
     <label>Find your film</label>
     <input type="text" name="" id="search-bar">
     <div class="dropdown">
-    <div class="dropdown-menu" id="dropdown-menu" role="menu">
-      <div id="target" class="dropdown-content results"></div>
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+        <div id="target" class="dropdown-content results"></div>
+        </div>
     </div>
-  </div>
+    <div id="summary"></div>
 `;
 
 const dropdown = document.querySelector(".dropdown");
@@ -49,6 +50,7 @@ const onInput = async event => {
         option.addEventListener("click", event => {
             dropdown.classList.remove("is-active");
             input.value = movie.Title;
+            onMovieSelection(movie);
         });
         document.getElementById("target").appendChild(option);
     }
@@ -59,5 +61,49 @@ document.addEventListener('click', event => {
         dropdown.classList.remove("is-active");
     }
 });
+
+const onMovieSelection = async(movie) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "80f0e4df",
+            i: movie.imdbID
+        }
+    });
+    console.log(response.data);
+    document.querySelector("#summary").innerHTML = movieDescTemplate(response.data);
+};
+
+const movieDescTemplate = (movieInfo) => {
+    return `
+        <article class="movie-desc">
+            <div class="movie-desc-upper">
+                <img class="movie-desc-poster" src=${movieInfo.Poster}>
+                <div>
+                    <h2 class="movie-desc-title">${movieInfo.Title} (${movieInfo.Year})</h2>
+                    <h4>${movieInfo.Genre}</h4>
+                    <p>${movieInfo.Plot}</p>
+                </div>
+            </div>
+            <div>
+                <div class="movie-desc-add">
+                    <h4>${movieInfo.BoxOffice}</h4>
+                    <span>Box office</span>
+                </div>
+                <div class="movie-desc-add">
+                    <h4>${movieInfo.Awards}</h4>
+                    <span>Awards</span>
+                </div>
+                <div class="movie-desc-add">
+                    <h4>${movieInfo.imdbRating}</h4>
+                    <span>IMDB Rating</span>
+                </div>
+                <div class="movie-desc-add">
+                    <h4>${movieInfo.Metascore}</h4>
+                    <span>Metascore</span>
+                </div>
+            </div>
+        </article>
+    `;
+};
 
 input.addEventListener("input", debounce(onInput, 500));
